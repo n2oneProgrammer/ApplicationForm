c7d4d62dd44046a3e3d11bed24b6cfe8489fd6d2
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-06-01 13:34:40 +0200

    init

---
+++ ApplicationForm/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationForm.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost\\MSSQLSERVER01;Database=MovieSystem;Trusted_Connection=True;TrustServerCertificate=True;");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
