fbee874e53ff1506b99172d2aea6a1a821553264
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-06-01 14:12:10 +0200

    Initial Data for Education Type

--- ApplicationForm/Models/ApplicationDbContext.cs
+++ ApplicationForm/Models/ApplicationDbContext.cs
@@ -9,6 +9,27 @@ public class ApplicationDbContext : DbContext
     public DbSet<EducationType> EducationTypes { get; set; }
     public DbSet<Internship> Internships { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EducationType>().HasData(
+            new EducationType
+            {
+                Id = 1,
+                Name = "Podstawowe"
+            },
+            new EducationType
+            {
+                Id = 2,
+                Name = "Średnie"
+            },
+            new EducationType
+            {
+                Id = 3,
+                Name = "Wyższe"
+            });
+        base.OnModelCreating(modelBuilder);
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
