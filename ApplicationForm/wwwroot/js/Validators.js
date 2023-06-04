e2b6db05b2cea5678a1345a2b9cfc8bebb61d98c
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-06-04 20:56:55 +0200

    Validators 3/3

--- ApplicationForm/wwwroot/js/Validators.js
+++ ApplicationForm/wwwroot/js/Validators.js
@@ -10,6 +10,15 @@ let checkFile = (file) => {
     const fileMb = fileSize / 1024 ** 2;
     return fileMb >= 2
 }
+//VALIDATOR STATES
+let nameOk = false;
+let surnameOk = false;
+let emailOk = false;
+let birthdayOk = false;
+let LMFileOk = true;
+let CVFileOk = true;
+let anotherFilesOk = true;
+let internshipOk = true;
 
 let attachedFileValidators = []
 let internshipValidators = []
@@ -30,20 +39,28 @@ window.addEventListener("load", () => {
     nameInput.on("change", () => {
         if (nameInput.val().length > 50) {
             nameError.text("Name is too long");
+            nameOk = false;
         } else if (nameInput.val().length === 0) {
             nameError.text("Name is required");
+            nameOk = false;
         } else {
-            nameError.text("")
+            nameError.text("");
+            nameOk = true;
         }
+        updateSubmit();
     });
     surnameInput.on("change", () => {
         if (surnameInput.val().length > 50) {
             surnameError.text("Surname is too long");
+            surnameOk = false;
         } else if (surnameInput.val().length === 0) {
             surnameError.text("Surname is required");
+            surnameOk = false;
         } else {
-            surnameError.text("")
+            surnameError.text("");
+            surnameOk = true;
         }
+        updateSubmit();
     });
 
     emailInput.on("change", () => {
@@ -51,17 +68,23 @@ window.addEventListener("load", () => {
 
         if (emailInput.val().match(validRegex)) {
             emailError.text("");
+            emailOk = true;
         } else {
-            emailError.text("Email is incorrect")
+            emailError.text("Email is incorrect");
+            emailOk = false;
         }
+        updateSubmit();
     });
 
     birthdayInput.on("change", () => {
         if (!isDateBeforeToday(birthdayInput.val())) {
             birthdayError.text("Birthday is not correct");
+            birthdayOk = false;
         } else {
-            birthdayError.text("")
+            birthdayError.text("");
+            birthdayOk = true;
         }
+        updateSubmit();
     });
 
     LMInput.on("change", () => {
@@ -69,37 +92,52 @@ window.addEventListener("load", () => {
             let f = LMInput.prop("files")[0];
             if (!["image/jpeg", "application/pdf", "application/msword"].includes(f.type)) {
                 LMError.text("File should be JPEG, PDF or MS word");
+                LMFileOk = false;
             } else if (checkFile(f)) {
                 LMError.text("File should be less than 2MB.");
+                LMFileOk = false;
             } else {
                 LMError.text("");
+                LMFileOk = true;
             }
         } else {
             LMError.text("");
+            LMFileOk = true;
         }
+        updateSubmit();
     });
 
     CVInput.on("change", () => {
         if (CVInput.prop("files").length > 0) {
             let f = CVInput.prop("files")[0];
-            if (checkFile(f)) {
+            if (!["image/jpeg", "application/pdf", "application/msword"].includes(f.type)) {
+                CVError.text("File should be JPEG, PDF or MS word");
+                CVFileOk = false;
+            } else if (checkFile(f)) {
                 CVError.text("File should be less than 2MB.");
+                CVFileOk = false;
             } else {
                 CVError.text("");
+                CVFileOk = true;
             }
         } else {
             CVError.text("");
+            CVFileOk = true;
         }
+        updateSubmit();
     });
-
+    updateSubmit();
 });
 let addAttachedFileValidator = (id) => {
     attachedFileValidators.push(id);
+    updateSubmit();
 }
 let removeAttachedFileValidator = (id) => {
     attachedFileValidators = attachedFileValidators.filter(a => a !== id);
+    updateSubmit();
 }
-let attachedFileValidatorCheck = (idChange=-1) => {
+let attachedFileValidatorCheck = (idChange = -1) => {
+    let ok = true;
     attachedFileValidators.forEach(file => {
         let fileInput = $("#attachedFile" + file);
         if (fileInput.prop("files").length > 0) {
@@ -108,8 +146,8 @@ let attachedFileValidatorCheck = (idChange=-1) => {
                 if (idChange === file) {
                     $("#attachedFileError" + file).text("File should be less than 2MB.");
                 }
+                ok = false;
             } else {
-
                 if (idChange === file) {
                     $("#attachedFileError" + file).text("");
                 }
@@ -118,18 +156,24 @@ let attachedFileValidatorCheck = (idChange=-1) => {
             if (idChange === file) {
                 $("#attachedFileError" + file).text("File field should not be empty");
             }
+            ok = false;
         }
-    })
+    });
+    anotherFilesOk = ok;
+    updateSubmit();
 }
 
 let addInternshipValidator = (id) => {
     internshipValidators.push(id);
+    updateSubmit();
 }
 let removeInternshipValidator = (id) => {
     internshipValidators = internshipValidators.filter(a => a !== parseInt(id));
+    updateSubmit();
 }
 
-let internshipValidatorCheck = (idChange=-1, inputType="") => {//input type = {Name, Start, End}
+let internshipValidatorCheck = (idChange = -1, inputType = "") => {//input type = {Name, Start, End}
+    let ok = true;
     internshipValidators.forEach(id => {
         let companyNameInput = $("#internship-company-name-" + id);
         let startInput = $("#internship-start-" + id);
@@ -143,12 +187,13 @@ let internshipValidatorCheck = (idChange=-1, inputType="") => {//input type = {N
             if (idChange === id && inputType === "Name") {
                 companyNameError.text("Company name is too long");
             }
+            ok = false;
         } else if (companyNameInput.val().length === 0) {
             if (idChange === id && inputType === "Name") {
                 companyNameError.text("Company name is required");
             }
+            ok = false;
         } else {
-
             if (idChange === id && inputType === "Name") {
                 companyNameError.text("");
             }
@@ -157,6 +202,7 @@ let internshipValidatorCheck = (idChange=-1, inputType="") => {//input type = {N
             if (idChange === id && inputType === "Start") {
                 startError.text("Date is not correct");
             }
+            ok = false;
         } else {
             if (idChange === id && inputType === "Start") {
                 startError.text("");
@@ -167,14 +213,35 @@ let internshipValidatorCheck = (idChange=-1, inputType="") => {//input type = {N
             if (idChange === id && inputType === "End") {
                 endError.text("Date is not correct");
             }
+            ok = false;
         } else if (!isDateLess(startInput.val(), endInput.val())) {
             if (idChange === id && inputType === "End") {
                 endError.text("End date should be after start date");
             }
+            ok = false;
         } else {
             if (idChange === id && inputType === "End") {
                 endError.text("");
             }
         }
-    })
+    });
+    internshipOk = ok;
+    updateSubmit();
+}
+
+let updateSubmit = () => {
+    console.log(nameOk, surnameOk, emailOk, birthdayOk, LMFileOk, CVFileOk, anotherFilesOk, internshipOk);
+    let submit = $("#submit")
+    if (nameOk &&
+        surnameOk &&
+        emailOk &&
+        birthdayOk &&
+        LMFileOk &&
+        CVFileOk &&
+        anotherFilesOk &&
+        internshipOk) {
+        submit.removeAttr("disabled");
+    } else {
+        submit.attr("disabled", true);
+    }
 }
